06bec3cc27ddd4eac0aba70d481fb2e297f8a255
Evgenii <52632749+Keanir@users.noreply.github.com>
2020-01-30 21:38:49 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,15 +7,15 @@ import fb from "firebase";
 import "firebase/firestore";
 
 Vue.config.productionTip = false;
-
+// Enter your Firebase Config below
 const firebaseConfig = {
-  apiKey: "AIzaSyA9HpfjJEOTQETSCM-iAX01icYWNvYiF74",
-  authDomain: "vue-simple-blog-f873e.firebaseapp.com",
-  databaseURL: "https://vue-simple-blog-f873e.firebaseio.com",
-  projectId: "vue-simple-blog-f873e",
-  storageBucket: "vue-simple-blog-f873e.appspot.com",
-  messagingSenderId: "18053521216",
-  appId: "1:18053521216:web:7bc8efaac0f9305943a4a5"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 fb.initializeApp(firebaseConfig);
